8e68620927522ba6906de2bcb8512f445fef307a
Sameera Dronavalli <sameeradronavalli327@gmail.com>
2026-01-12 12:30:47 +0530

    Day 1 & Day 2: Node.js basics and backend mindset

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 // Day-1:
 // installed node js
 // run  the terminal
-
-
 console.log("hello, this is first node program")
+
+// Day 2 Notes:
+// Node.js runs JavaScript on the server
+// Backend handles logic, not UI
+console.log("Frontend handles UI");
+console.log("Backend handles logic and data");
