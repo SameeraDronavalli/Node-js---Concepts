68775f44dd95388e274143d03f2495bb4372ad05
Sameera Dronavalli <sameeradronavalli327@gmail.com>
2026-01-12 15:28:25 +0530

    changed variable name

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ console.log("Backend handles logic and data");
 //using npm installed package.json  => It has Project details + dependencies list
 //Express allows us to easily create HTTP servers and define routes for handling client requests
 const express = require("express");
-const server = express();
-server.get("/", (req, res) => {
+const app = express();
+app.get("/", (req, res) => {
     res.send("Here is the response for the request")
 });
-server.listen(3000,()=>{
+app.listen(3000,()=>{
     console.log("server started on port 3000")
 })
